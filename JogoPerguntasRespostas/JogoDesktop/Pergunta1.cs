853114d5ab6a02f83f1dc70698d2c9f74100adc3
Tiago Assis <tiago.assis@aedu.com>
2017-02-06 14:45:08 -0200

    -Atualizado;

--- JogoPerguntasRespostas/JogoDesktop/Pergunta1.cs
+++ JogoPerguntasRespostas/JogoDesktop/Pergunta1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace JogoDesktop
 {
@@ -22,7 +23,36 @@ namespace JogoDesktop
             if (rdbResposta4.Checked == true)
             {
                 MessageBox.Show("ACERTOU");
-            }
+
+                string path = "Server=AME0556342W10-1\\SQLEXPRESS;Database=db_PerguntasERespostas;Trusted_Connection=Yes";
+                SqlConnection conexao = new SqlConnection(path);
+
+                SqlCommand comando = new SqlCommand();
+
+                comando.Connection = conexao;
+
+                string insert = "INSERT INTO tb_perguntas";
+                insert += "(pergunta, resposta_correta)";
+                insert += " VALUES ";
+                insert += " ( ";
+
+                insert += " '" + lblPergunta.Text + "' ,";
+                insert += " '" + rdbResposta4.Text + "' ";
+
+                insert += " ) ";
+
+                    
+                conexao.Open();
+                comando.CommandText = insert;
+                comando.ExecuteNonQuery();
+                conexao.Close();
+
+
+                
+             }
+                
+                    
+
             else
             {
                 MessageBox.Show("SEU BURRO");
